59aa65b06ebcf25eedde220a8a7d848cf1800a66
Nick <nick.degruccio@gmail.com>
2023-06-11 19:47:23 -0700

    Minor adjustments to build configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     id("impactor.root-conventions")
     id("architectury-plugin") version "3.4-SNAPSHOT" apply false
     id("dev.architectury.loom") version "0.12.0-SNAPSHOT" apply false
+    id("org.spongepowered.gradle.vanilla") version "0.2.1-SNAPSHOT" apply false
 }
 
 group = "net.impactdev.impactor"
--- minecraft/build.gradle.kts
+++ minecraft/build.gradle.kts
@@ -1,48 +1,5 @@
 plugins {
     id("impactor.base-conventions")
-    id("impactor.publishing-conventions")
-    id("org.spongepowered.gradle.vanilla") version("0.2.1-SNAPSHOT")
 }
 
-repositories {
-    mavenCentral()
-    maven("https://oss.sonatype.org/content/repositories/snapshots")
-}
 
-minecraft {
-    version("${rootProject.property("minecraft")}")
-}
-
-java {
-    toolchain {
-        languageVersion.set(JavaLanguageVersion.of(17))
-    }
-}
-
-dependencies {
-    api(project(":impactor"))
-    api(project(":api:items"))
-    api(project(":api:ui"))
-
-    api(project(":minecraft:api"))
-}
-
-publishing {
-    publications {
-        create<MavenPublication>(project.name) {
-            from(components["java"])
-
-            groupId = "net.impactdev.impactor.integration"
-            artifactId = "minecraft"
-
-            val plugin = rootProject.property("plugin").toString()
-            val minecraft = rootProject.property("minecraft").toString()
-            val snapshot = rootProject.property("snapshot") == "true"
-
-            version = "${plugin}+${minecraft}"
-            if(snapshot) {
-                version += "-SNAPSHOT"
-            }
-        }
-    }
-}
